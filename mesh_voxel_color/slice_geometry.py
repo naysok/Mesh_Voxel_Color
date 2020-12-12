07a5f5b24e299e27c1f199fe8f2c21ab9746b771
naysok_win10 <ysok.ysok@gmail.com>
2020-12-12 19:21:45 +0900

    [add] slice_geometry

---
+++ mesh_voxel_color/slice_geometry.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("C:\\Users\\ysoky\\Documents\\Mesh_Voxel_Color\\_module_\\Mesh_Contour")
+
+from mesh_contour import run_mesh_contour
+mc = run_mesh_contour.RunMeshContour()
+
+
+class SliceGeometry():
+
+
+    def get_meshes_and_ranges(self, file_path):
+        meshes, ranges = mc.set_mesh(file_path)
+        return meshes, ranges
+
+
+    def contour_meshes(self, meshes, ranges, height):
+        meshes_filtered, lines = mc.run_contour(meshes, ranges, height)
+        return lines
