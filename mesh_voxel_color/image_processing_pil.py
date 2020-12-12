7ff8796d47c9d3ab71eda0e2efa9bdc9b6448c73
naysok_win10 <ysok.ysok@gmail.com>
2020-12-12 21:29:44 +0900

    [update] Contour, Convert + Remap

--- mesh_voxel_color/image_processing_pil.py
+++ mesh_voxel_color/image_processing_pil.py
@@ -2,6 +2,10 @@ import math
 from PIL import Image, ImageDraw, ImageOps, ImageEnhance
 
 
+from .import util
+ut = util.Util()
+
+
 class ImageProcessingPIL():
 
 
@@ -23,7 +27,7 @@ class ImageProcessingPIL():
 
 
     def create_canvas(self, canvas_size):
-        new = Image.new("RGBA", (canvas_size, canvas_size), (0, 0, 0, 0))
+        new = Image.new("RGB", (canvas_size, canvas_size), (255, 255, 255))
         return new
 
 
@@ -37,31 +41,68 @@ class ImageProcessingPIL():
     ##############################
 
 
-    def convert_vert2s_to_lines(self, vert2s):
+    def convert_vert2_to_line(self, canvas_size, volume_size, vert2):
         
+        ### vert2 = [vert, vert]
+        ### vert2 = [[x0, y0, z0], [x1, y1, z1]]
+        
+        p0 = vert2[0]
+        p1 = vert2[1]
+
+        _v0 = p0[0]
+        _v1 = p0[1]
+        _v2 = p1[0]
+        _v3 = p1[1]
+        
+        ### Clac Resolution
+        v0 = ut.remap_number(_v0, 0, volume_size, 0, canvas_size)
+        v1 = ut.remap_number(_v1, 0, volume_size, 0, canvas_size)
+        v2 = ut.remap_number(_v2, 0, volume_size, 0, canvas_size)
+        v3 = ut.remap_number(_v3, 0, volume_size, 0, canvas_size)
+
+        line = [v0, v1, v2, v3]
+
+        return line
+
+
+    def convert_vert2s_to_lines(self, canvas_size, volume_size, vert2s):
+
+        ### vert2s = [vert2, vert2, , , , , , vert2]
         ### vert2s = [[[x0, y0, z0], [x1, y1, z1]], , , , , , [[xm, ym, zm], [xn, yn, zn]]]
-        pass
+        
+        lines = []
+
+        for i in range(len(vert2s)):
+            vert2 = vert2s[i]
+            line = self.convert_vert2_to_line(canvas_size, volume_size, vert2)
+            lines.append(line)
+
+        return lines
 
 
     def draw_line(self, canvas_draw, line):
 
-        canvas_draw.line((line), fill=(0, 0, 0), width = 2)
+        ### Color : Black
+        ### Line Width : 2
+
+        canvas_draw.line((line), fill = (0, 0, 0), width = 2)
         return canvas_draw
 
 
-    def draw_lines(self, canvas, lines):
+    def draw_lines(self, canvas, canvas_size, volume_size, lines):
 
         ### Lines = vert2s
         ### vert2s = [vert2, vert2, , , , , , vert2]
         ### vert2s = [[[x0, y0, z0], [x1, y1, z1]], , , , , , [[xm, ym, zm], [xn, yn, zn]]]
         
-        lines = [[0, 0, 200, 200], [200, 0, 200, 200]]
+        ### Convert (vert2s >> lines) + Clac Resolution
+        lines_draw = self.convert_vert2s_to_lines(canvas_size, volume_size, lines)
 
         ### Convert Canvas to Draw
         canvas_draw = ImageDraw.Draw(canvas)
 
-        for i in range(len(lines)):
-            line = lines[i]
+        for i in range(len(lines_draw)):
+            line = lines_draw[i]
             self.draw_line(canvas_draw, line)
 
         return canvas_draw
@@ -70,16 +111,14 @@ class ImageProcessingPIL():
     ########################################
 
 
-    def draw_contours(self, prj_path, canvas_size, index, lines):
+    def draw_contours(self, prj_path, canvas_size, volume_size, index, lines):
 
         ### Create Canvas
         canvas = self.create_canvas(canvas_size)
         
         ### Draw Lines
-        self.draw_lines(canvas, None)
-
-        ### canvas.show()
+        self.draw_lines(canvas, canvas_size, volume_size, lines)
 
         ### Save Canvas
-        export_path = prj_path + "image_0\\image_{}.png".format(str(int(index))) 
+        export_path = prj_path + "image_0\\image_{}.jpg".format(str(int(index))) 
         self.export_image(canvas, export_path)
