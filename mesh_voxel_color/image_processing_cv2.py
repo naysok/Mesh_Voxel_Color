020432b9848658667629386896b9f98f28a73ce9
naysok_win10 <ysok.ysok@gmail.com>
2020-12-22 00:30:54 +0900

    [upadte] Segment Inside/Outside

--- mesh_voxel_color/image_processing_cv2.py
+++ mesh_voxel_color/image_processing_cv2.py
@@ -30,36 +30,78 @@ class ImageProcessingCV2():
         return img
 
 
+    def canvas_segment(self, channel):
+
+        new_channel = ((channel + 2)%4 == 0).astype(np.uint8)
+        new_channel = new_channel * 255
+
+        return new_channel
+
+
+    def boolean_canvas(self, canvas_all):
+        
+        for i in range(len(canvas_all)):
+
+            bb, gg, rr = cv2.split(canvas_all[i])
+
+            if i == 0:
+                boolean_ = bb
+            
+            else:
+                boolean_ += bb
+        
+        canvas_seg = self.canvas_segment(boolean_)
+
+        img_segment = cv2.merge((canvas_seg, canvas_seg, canvas_seg))
+
+        return img_segment
+
+
     def find_hierarchy(self, img):
 
-        k = 2
-        kernel = np.ones((k, k), np.uint8)
-        erosion = cv2.erode(img, kernel, iterations = 1)
+        h, w, c = img.shape
         
-        gray = cv2.cvtColor(erosion, cv2.COLOR_RGB2GRAY) 
+        ### 
+        k0 = 3
+        kernel = np.ones((k0, k0), np.uint8)
+        erosion = cv2.erode(img, kernel, iterations = 1)
+
+        ### Gray Scale
+        gray = cv2.cvtColor(erosion, cv2.COLOR_RGB2GRAY)
 
         ### Show
-        # cv2.imshow("image", gray)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        cv2.imshow("image", gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
+        ### opencv 4
         ### https://qiita.com/rareshana/items/6a2f5e7396f28f6eee49
 
         contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         # print("contours=",len(contours),  "hierarchy=",len(hierarchy))
-        print(hierarchy)
+        # print(hierarchy)
 
-        ### contours[0] : Image Edge
-        ### contours[1] : Contour-Outside
-        ### contours[2] : Contour-Inside
+        ### Nesting
+        hierarchy = hierarchy[0]
 
-        cv2.drawContours(img, contours[0], -1,(255,  0,  0),3)
-        cv2.drawContours(img, contours[1], -1,(  0,255,  0),3)
-        cv2.drawContours(img, contours[2], -1,(  0,  0,255),3)
+        ### Segment
+        canvas_all = []
 
+        for i in range(len(contours)):
+
+            ### contours[0] : Image Edge
+            if i != 0:
+
+                canvas = self.create_image(w)
+                cnt = contours[i]
+                cv2.fillConvexPoly(canvas, cnt, (1, 0, 0))
+
+                canvas_all.append(canvas)
+
+        img_boolean = self.boolean_canvas(canvas_all)
 
         ### Show
-        cv2.imshow("image", img)
+        cv2.imshow("image", img_boolean)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
