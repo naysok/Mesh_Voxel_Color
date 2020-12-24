b68e4be5c0d02029627f8aa29e19f7845d4e7687
naysok_win10 <ysok.ysok@gmail.com>
2020-12-25 00:19:39 +0900

    [update] ver1

--- mesh_voxel_color/color_pil.py
+++ mesh_voxel_color/color_pil.py
@@ -53,6 +53,17 @@ class ColorPIL():
         return pts
 
 
+    def get_points_from_stl_np(self, file_path):
+    
+        ### Point From STL
+        pts = sp.stl2points(file_path)
+        pts_format = [pts]
+        # print(pts_format)
+
+        pts_np = np.array(pts_format)
+        return pts_np
+
+
     def calc_distance_min(self, pos, pts):
 
         pos_n = np.array(pos)
@@ -67,6 +78,22 @@ class ColorPIL():
         return d_min
 
 
+    def clac_all_distance(self, pos, pts):
+
+        v = pos - pts
+        # print("v.shape :", v.shape)
+
+        vt = v.T
+
+        d = np.sqrt((vt[0] * vt[0]) + (vt[1] * vt[1]) + (vt[2] * vt[2]))
+        # print("d.shape :", d.shape)
+
+        dm_np = np.amin(d, axis=0)
+        # print("dm.shape :", dm_np.shape)
+
+        return dm_np
+
+
     ########################################
 
 
@@ -77,7 +104,7 @@ class ColorPIL():
     #####################
 
 
-    def scan_image_calc_color(self, file_path, height, pts):
+    def scan_image_calc_color(self, file_path, height, pts_np):
 
         ### Open Image
         img = self.open_image(file_path)
@@ -89,28 +116,81 @@ class ColorPIL():
         px_length = len(px)
 
         ### Generate Distance-List
-
         print("Distance")
+        
+        px_list = []
+        for i in range(w):
+            for j in range(h):
+                px_list.append([[i, j, height]])
+        
+        ### pos-numpy array (from Image)
+        pos_np = np.array(px_list)
+        # print("pos.shape :", pos_np.shape)
+
+        ### Process
+        SPLIT = 1000
+        pos_np_split = np.array_split(pos_np, SPLIT)
+        # print(len(pos_np_split))
+
+        dist_tmp = []
+
+        for i in range(SPLIT):
+
+            tmp_p = pos_np_split[i]
+            # print("pts.shape :", tmp_p.shape)
+
+            ### pts-numpy array (from STL)
+            # print("pts.shape :", pts_np.shape)
+
+            ### 
+            d = self.clac_all_distance(tmp_p, pts_np)
+            dist_tmp.append(d)
+
+        dist_list = np.concatenate(dist_tmp, 0)
+        print(len(dist_list))
+        
+        # return None
+
+
+        # self.clac_all_distance(pos)
+
+        """
 
         dist = []
 
         for i in range(px_length):
 
-            print(i)
+            if i%200 == 0:
+                
+                ############################################################
+                # print(i)
 
-            rr, gg, bb = px[i]
+                rr, gg, bb = px[i]
 
-            ### Process
-            ### Calc Distance
-            if rr > 127:
+                ### Process
+                ### Calc Distance
+                if rr > 127:
 
-                u, v = divmod(px_length, i)
-                d = self.calc_distance_min([u, v, height], pts)
-                dist.append(d)
-            
-            ### None
+                    if i == 0:
+                        u, v = [0, 0]
+                        d = self.calc_distance_min([u, v, height], pts)
+                        dist.append(d)
+
+                    else:
+                        u, v = divmod(px_length, i)
+                        d = self.calc_distance_min([u, v, height], pts)
+                        dist.append(d)
+                ############################################################
+                
+                ### None
+                else:
+                    dist.append(0)
+                
+            ### None (dev)
             else:
                 dist.append(0)
+
+        # print(dist)
 
         ### Generate Color From Distance
         
@@ -121,7 +201,7 @@ class ColorPIL():
         d_min = min(dist)
         d_max = max(dist)
 
-        th = 100
+        th = 1000
 
         for j in range(px_length):
             d = dist[j]
@@ -131,14 +211,17 @@ class ColorPIL():
                 result_px.append((0, 0, 0, 0))
 
             elif d > th:
-                result_px.append((255, 255, 255, 255))
+                result_px.append((255, 0, 0, 255))
 
             else:
-                c = int(ut.remap_number(d, 0, 100, 0, 255))
-                result_px.append((c, c, c, 255))
+                c = int(ut.remap_number(d, 0, th, 0, 255))
+                # result_px.append((0, c, 0, 255))
+                result_px.append((0, 0, 255, 255))
 
 
         # ### Generate New Image
         result_img.putdata(tuple(result_px))
 
         return result_img
+
+        """
