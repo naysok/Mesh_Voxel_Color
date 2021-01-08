db51b5daf27bd904fc49a65dde042284f3ffafa8
naysok_win10 <ysok.ysok@gmail.com>
2021-01-08 18:26:47 +0900

    [update] PrepareWorkingDirectpry

--- mesh_voxel_color/util.py
+++ mesh_voxel_color/util.py
@@ -8,6 +8,24 @@ class Util():
         return ((src - old_min) / (old_max - old_min) * (new_max - new_min) + new_min)
 
 
+    def hsv2rgb(self, color):
+        pass
+
+
+    def rgb2hsv(self, color):
+        pass
+
+
+    ################################################################################
+
+
+    #########################################
+    ###                                   ###
+    ###     Prepare Working Directory     ###
+    ###                                   ###
+    #########################################
+
+
     def mkdir_prj(self, dir_path, prj_name):
 
         ### Check and Create Dirctory
@@ -83,6 +101,36 @@ class Util():
             print("Create Directory : {}".format(name))
 
 
+    def mkdir_image_4(self, prj_path):
+
+        ### Check and Create Dirctory
+        dirs = []
+        name = "image_4"
+
+        for i in os.listdir(prj_path):
+            dirs.append(i)
+        dir_new = prj_path + name
+
+        if name not in dirs:
+            os.mkdir(dir_new)
+            print("Create Directory : {}".format(name))
+
+
+    def mkdir_image_5(self, prj_path):
+
+        ### Check and Create Dirctory
+        dirs = []
+        name = "image_5"
+
+        for i in os.listdir(prj_path):
+            dirs.append(i)
+        dir_new = prj_path + name
+
+        if name not in dirs:
+            os.mkdir(dir_new)
+            print("Create Directory : {}".format(name))
+
+
     def prepare_prj_dir(self, dir_path, prj_name):
 
         ### Run (Initialize)
@@ -93,3 +141,8 @@ class Util():
         self.mkdir_image_1(prj_path)
         self.mkdir_image_2(prj_path)
         self.mkdir_image_3(prj_path)
+        self.mkdir_image_4(prj_path)
+        self.mkdir_image_5(prj_path)
+
+
+    ################################################################################
